5d89220b31383577d485d51aadbc6dfa2b5105ca
adellhk & Jamal Moon <jmoon018@ucr.edu>
2015-02-25 16:43:23 -0800

    adds routing in one file with angular

--- public/application.js
+++ public/application.js
@@ -1,11 +1,34 @@
+var app = angular.module('ParOverflow', ['ngRoute']);
 
-var app = angular.module('ParOverflow', [])
+app.config(['$routeProvider',
+  function ($routeProvider) {
+    console.log("hitting the route provider")
+    $routeProvider.
+      when('/testRoute1/:message', {
+        templateUrl: "test1.html",
+        controller: "testCtrl1"
+      }).
+      when("/testRoute2", {
+        templateUrl: "test2.html",
+        controller: "testCtrl2"
+      }).
+      when("/questions", {
+        templateUrl: "questionsIndex.html",
+        controller: "QuestionsIndexCtrl"
+      }).
+      when("/questions/:id", {
+        templateUrl: "questionsShow.html",
+        controller: "QuestionsShowCtrl"
+      }).
+      otherwise({
+        redirectTo: "/index.html"
+      });
+  }
+]);
 
-app.controller('HomeCtrl', function($scope, $http){
-
-
+app.controller('QuestionsIndexCtrl', function($scope, $http){
   $scope.getQuestions = function() {
-    return $http.get('/api/questions').
+    $http.get('/api/questions').
     success(function(data) {
       $scope.questions = data
     }).
@@ -15,12 +38,36 @@ app.controller('HomeCtrl', function($scope, $http){
   }
   $scope.getQuestions();
 
+  $scope.editorShown = false
+  $scope.showEditor = function () {
+    $scope.editorShown = !$scope.editorShown;
+  }
+
+  $scope.getAnswers = function(id) {
+    console.log("GETTING THE ANSWER FROM Q: " + id);
+    id = $scope.questions[id].id
+    console.log("new id: " + id)
+    $http.get("/api/questions/" + id + "/answers").
+      success(function(data){
+        console.log('sucess! data:' +data)
+        $scope.answers = data
+      }).
+      error(function(data) {
+        console.log(data);
+        console.log("getAnswers failed");
+      })
+  }
+
 });
 
-//   $scope.getQuestions().then(function(res){
-//     console.log(res.data);
-//   }, function(){
-//     console.log("it failed")
-//   })
 
-// })
+app.controller("testCtrl1", function($scope, $routeParams) {
+  console.log("HITTING THE TEST CONTROLLER 1");
+  $scope.message = "and now a message from the anchor tag: "
+    + $routeParams["message"];
+});
+
+app.controller("testCtrl2", function($scope, $routeParams) {
+  console.log("HITTING THE TEST CONTROLLER 2");
+  $scope.message = "YOU ARE A GOD 2";
+});
